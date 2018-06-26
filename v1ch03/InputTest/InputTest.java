c69a4107e78e42ef63ab27c29d903533a77b8830
richard.guo <guozepeng@aliyun.com>
2018-06-26 10:08:08 +0800

    传入java核心技术示例代码

---
+++ v1ch03/InputTest/InputTest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+ * This program demonstrates console input.
+ *
+ * @author Cay Horstmann
+ * @version 1.10 2004-02-10
+ */
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // get first input
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        // get second input
+        System.out.print("How old are you? ");
+        int age = in.nextInt();
+
+        // display output on console
+        System.out.printf("Hello, %s. Next year, you'll be %d", name, age + 1);
+    }
+}
